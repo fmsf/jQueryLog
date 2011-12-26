2c224bf8afc847912f3597f7d2ef6c9efe577682
Francisco Ferreira <francisco.m.s.ferreira@gmail.com>
2011-12-26 14:06:38 +0100

    added license to jquery log.

--- jquery.log.js
+++ jquery.log.js
@@ -1,3 +1,29 @@
+/*
+* MIT License
+*
+* Copyright (c) 2011 Francisco M.S. Ferreira
+* http://www.jquerylog.com
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*
+*/
+
 (function(){
 		var c = console,
 			indentLevel = 0,
@@ -19,7 +45,7 @@
 		$ = function(selector, context){
 			indentLevel = 0;
 			var returned = $jq(selector,context);
-			log("$",(context===undefined ? selector : selector+" -> "+ context), returned);
+			log("\n$",(context===undefined ? selector : selector+" -> "+ context), returned);
 			return returned;
 		};
 		
@@ -90,7 +116,7 @@
 					return returned;
 				};
 				executed[name]= true;
-				console.log("applied: "+name);
+				//console.log("applied: "+name);
 			})();
 		}
 		
